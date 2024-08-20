e191936736f146518d6d2cd6949c3b0298e08f75
algonouir <algo.mahdi.nouri@gmail.com>
2024-08-20 21:15:30 +0330

    ...

--- venv/bin/runxlrd.py
+++ venv/bin/runxlrd.py
@@ -1,4 +1,4 @@
-#!/home/nora/Forteenall/infApps/dopamin/django/venv/bin/python3.11
+#!/home/nora/projects/coffe-cup/django/venv/bin/python
 # Copyright (c) 2005-2012 Stephen John Machin, Lingfo Pty Ltd
 # This script is part of the xlrd package, which is released under a
 # BSD-style licence.
